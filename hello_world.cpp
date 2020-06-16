5c62f78cfb6da1c28a876ef52db6154c8274aa19
fdrt29 <fdrt29@gmail.com>
2020-06-16 15:01:57 +0300

    fix using namespace

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
-using namespace std;
+
 int main(){
-string user;
-cin>>user;
-cout<<"Hello world from"<<user<<endl;
+std::string user;
+std::cin>>user;
+std::cout<<"Hello world from"<<user<<std::endl;
 return 0;
 }
