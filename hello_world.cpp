505d7157e7d01025f3cde9f8117752d738352654
fdrt29 <fdrt29@gmail.com>
2020-06-16 15:25:59 +0300

    fix code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
 
-int main(){
-std::string user;
-std::cin>>user; // ввод имя пользователя
-std::cout<<"Hello world from"<<user<<std::endl;
-return 0;
+int main()
+{
+  std::string user;
+  std::cin >> user; // input username
+  std::cout << "Hello world from" << user << std::endl;
+  return 0;
 }
